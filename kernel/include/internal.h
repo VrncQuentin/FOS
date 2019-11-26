9603d8883797c64bce1ef11c63e24a88098c2264
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2019-11-26 23:21:43 +0100

    [Added]     low level C functions to read/write from/to the given port (usually screen's port)     Beginning of screen's driver.

--- kernel/include/internal.h
+++ kernel/include/internal.h
@@ -1,7 +1,6 @@
 #ifndef _INTERNAL_H_
 #define _INTERNAL_H_
 
-#undef VIDEO_MEM
-#define VIDEO_MEM (0xb8000)
+#include <inttypes.h>
 
 #endif /* _INTERNAL_H_ */
---
+++ kernel/low_level_io.c
@@ -0,0 +1,67 @@
+#include "internal.h"
+
+/**
+ * @fn uint8_t read8_port(uint16_t port)
+ * @brief Reads a byte from port.
+ *
+ * @param[in]  port Port to be read.
+ * @param[out] b    Byte read.
+ *
+ * "=a" (b)   means store 'al' to 'b' when done.
+ * "d" (port) means load 'port' into 'edx'.
+ */
+uint8_t read8_port(uint16_t port)
+{
+    uint8_t b;
+
+    __asm__("in %%dx, %%al" : "=a" (b) : "d" (port));
+    return b;
+}
+
+/**
+ * @fn uint16_t read16_port(uint16_t port)
+ * @brief Reads a word from port.
+ *
+ * @param[in]  port Port to be read.
+ * @param[out] w    Word read.
+ *
+ * "=a" (w)   means store 'eax' to 'w' when done.
+ * "d" (port) means load 'port' into 'edx'.
+ */
+uint16_t read16_port(uint16_t port)
+{
+    uint16_t w;
+
+    __asm__("in %%dx, %%ax" : "=a" (w) : "d" (port));
+    return w;
+}
+
+/**
+ * @fn void write8_port(uint16_t port, uint8_t data)
+ * @brief Write a byte to port.
+ *
+ * @param[in] port Port to which we'll write.
+ * @param[in] data Data to be written to port.
+ *
+ * "a" (data) means load 'data' to 'al'.
+ * "d" (port) means load 'port' to 'edx'.
+ */
+void write8_port(uint16_t port, uint8_t data)
+{
+    __asm__("out %%al, %%dx": : "a" (data) : "d" (port));
+}
+
+/**
+ * @fn void write16_port(uint16_t port, uint16_t data)
+ * @brief Write a byte to port.
+ *
+ * @param[in] port Port to which we'll write.
+ * @param[in] data Data to be written to port.
+ *
+ * "a" (data) means load 'data' to 'eax'.
+ * "d" (port) means load 'port' to 'edx'.
+ */
+void write16_port(uint16_t port, uint16_t data)
+{
+    __asm__("out %%ax, %%dx": : "a" (data) : "d" (port));
+}
